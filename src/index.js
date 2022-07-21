08197c4623e4d49f70eb30df25b03ef09473449e
Aleksei Stoyanenko <stoynalex@gmail.com>
2022-07-21 19:28:48 +0500

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let x = [];
+  for (let i = 0; i < str.length; i++) {
+    x.push(str[i]);
+  }
+  for (let i = 0; i < x.length; i++) {
+    bracketsConfig.map((a) => {
+      if (x[i] === a[0] && x[i + 1] === a[1]) {
+        x.splice(i, 2);
+        i = i - 2;
+      }
+    });
+  }
+  return x.length === 0;
+};
